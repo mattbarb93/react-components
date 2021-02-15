f61f8ebabb44b2bbc8bcd15d4fdaac807ba19558
Matheus Barbosa <mattbarb93@gmail.com>
2021-02-15 15:36:55 -0500

    Created CommentDetail as a component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+        <div className="ui container comments">
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+        </div>
+    )
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
