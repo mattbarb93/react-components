280b95d8f17ccbe392a63ec873493d6254855d0f
Matheus Barbosa <mattbarb93@gmail.com>
2021-02-15 16:01:49 -0500

    Props system added

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail author="Sam" timeAgo="Today at 4 PM" text="yabadabadoo" image={faker.image.image()}/>
+            <CommentDetail author="Alex" timeAgo="Today at 2 AM" text="ka;ksdmas;d" image={faker.image.image()}/>
+            <CommentDetail author="Jack" timeAgo="Today at 2 PM" text="woooga" image={faker.image.image()}/>
         </div>
     )
 };
